71f7d295d6fc7e8087943a785e24cf1447188c70
MariemCheriff <164797732+MariemCheriff@users.noreply.github.com>
2024-04-28 18:46:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>adamindan.com</title>
+    <title>nosvoitures.com</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
@@ -38,14 +38,14 @@
 </head>
 
 <body>
-    <!-- Topbar Start -->
-    <div class="container-fluid bg-dark py-3 px-lg-5 d-none d-lg-block">
+       <!-- Topbar Start -->
+       <div class="container-fluid bg-dark py-3 px-lg-5 d-none d-lg-block">
         <div class="row">
             <div class="col-md-6 text-center text-lg-left mb-2 mb-lg-0">
                 <div class="d-inline-flex align-items-center">
-                    <a class="text-body pr-3" href=""><i class="fa fa-phone-alt mr-2"></i>+90 538 200 44 66 </a>
+                    <a class="text-body pr-3" href=""><i class="fa fa-phone-alt mr-2"></i>+216 99 485 200 </a>
                     <span class="text-body">|</span>
-                    <a class="text-body px-3" href=""><i class="fa fa-envelope mr-2"></i>adamindan@adamindan.com</a>
+                    <a class="text-body px-3" href=""><i class="fa fa-envelope mr-2"></i>Nosvoitures@gmail.com</a>
                 </div>
             </div>
             <div class="col-md-6 text-center text-lg-right">
@@ -72,43 +72,45 @@
     <!-- Topbar End -->
 
 
+
     <!-- Navbar Start -->
     <div class="container-fluid position-relative nav-bar p-0">
         <div class="position-relative px-lg-5" style="z-index: 9;">
             <nav class="navbar navbar-expand-lg bg-secondary navbar-dark py-3 py-lg-0 pl-3 pl-lg-5">
-                <a href="" class="navbar-brand">
-                    <h1 class="text-lowercase text-primary mb-1">adamindan.com</h1>
+                <a href="index.php" class="navbar-brand">
+                    <h1 class="text-lowercase text-primary mb-1">Nosvoitures@gmail.com </h1> 
                 </a>
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
                     <div class="navbar-nav ml-auto py-0">
-                        <a href="index.php" class="nav-item nav-link active">EV</a>
-                        <a href="about.html" class="nav-item nav-link">Hakkımızda</a>
-                        <a href="service.html" class="nav-item nav-link">Servis</a>
+                        <a href="index.php" class="nav-item nav-link active">Accueil</a>
+                        <a href="about.html" class="nav-item nav-link">A propos</a>
+                        <a href="service.html" class="nav-item nav-link">Services</a>
                         <div class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Arabalar</a>
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Les voitures</a>
                             <div class="dropdown-menu rounded-0 m-0">
-                                <a href="car.html" class="dropdown-item">Araç Listeleme</a>
-                      
+                                <a href="car.html" class="dropdown-item">Liste des voitures</a>
+                                
                             </div>
                         </div>
                         <div class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Sayfalar</a>
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
                             <div class="dropdown-menu rounded-0 m-0">
-                                <a href="team.html" class="dropdown-item">Biz Kimiz</a>
-                                <a href="testimonial.html" class="dropdown-item">Kullanıcı Yorumları</a>
+                                <a href="team.html" class="dropdown-item">Qui sommes nous ?</a>
+                                <a href="testimonial.html" class="dropdown-item">Avis des utilisateurs</a>
                             </div>
                         </div>
-                        <a href="contact.php" class="nav-item nav-link">Bize Ulaşın</a>
-                        <a href="loginpage.php" class="nav-item nav-link">Giriş Yap</a>
+                        <a href="contact.html" class="nav-item nav-link">Contancez-nous</a>
+                        <a href="http://localhost/demo/adamindan.com/loginpage.php" class="nav-item nav-link">S'identifier</a>
                     </div>
                 </div>
             </nav>
         </div>
     </div>
     <!-- Navbar End -->
+
 
 
 
@@ -645,80 +647,79 @@
     <!-- Vendor End -->
 
     <!-- Footer Start -->
-    <div class="container-fluid bg-secondary py-5 px-sm-3 px-md-5" style="margin-top: 90px;">
-        <div class="row pt-5">
-            <div class="col-lg-3 col-md-6 mb-5">
-                <h4 class="text-uppercase text-light mb-4">Bize Ulaşın</h4>
-                <p class="mb-2"><i class="fa fa-map-marker-alt text-white mr-3"></i>Halkalı Cad. No 281, Halkalı,<br> Küçükçekmece, İSTANBUL 34303</p>
-                <p class="mb-2"><i class="fa fa-phone-alt text-white mr-3"></i>+90 553 382 71 91</p>
-                <p><i class="fa fa-envelope text-white mr-3"></i>adamindan@adamindan.com
-                </p>
-                <h6 class="text-uppercase text-white py-2">Sosyal Medyada Biz</h6>
-                <div class="d-flex justify-content-start">
-                    <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="https://twitter.com/aitchkays"><i class="fab fa-twitter"></i></a>   
-                    <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="https://www.facebook.com/profile.php?id=100018305796102&mibextid=LQQJ4d"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="https://www.linkedin.com/in/halil-k%C3%B6se-7017bb1b4/"><i class="fab fa-linkedin-in"></i></a>
-                    <a class="btn btn-lg btn-dark btn-lg-square" href="https://www.instagram.com/aitchkays/""><i class="fab fa-instagram"></i></a>
-                </div>
-            </div>
-
-
-
-             <div class="col-lg-3 col-md-6 mb-5">
-                <h4 class="text-uppercase text-light mb-4">Meraklısına </h4>
-                <div class="d-flex flex-column justify-content-start">
-                    <a class="text-body mb-2" href="http://localhost/demo/adamindan.com/contact.php"><i class="fa fa-angle-right text-white mr-2"></i>Gizlilik</a>
-                    <a class="text-body mb-2" href="http://localhost/demo/adamindan.com/contact.php"><i class="fa fa-angle-right text-white mr-2"></i>Şartlarımız</a>
-                    <a class="text-body mb-2" href="http://localhost/demo/adamindan.com/contact.php"><i class="fa fa-angle-right text-white mr-2"></i>Yeni Üye Kaydı</a>
-                    <a class="text-body mb-2" href="http://localhost/demo/adamindan.com/contact.php"><i class="fa fa-angle-right text-white mr-2"></i>İade İşlemleri</a>
-                    <a class="text-body" href="http://localhost/demo/adamindan.com/contact.php"><i class="fa fa-angle-right text-white mr-2"></i>Sıkça Sorulan</a>
-                </div>
-            </div>            <div class="col-lg-3 col-md-6 mb-5">
-                <h4 class="text-uppercase text-light mb-4">Galeri</h4>
-                <div class="row mx-n1">
-                    <div class="col-4 px-1 mb-2">
-                        <a href=""><img class="w-100" height="100px" width="100px" src="img/gallery-1.jpg" alt=""></a>
-                    </div>
-                    <div class="col-4 px-1 mb-2">
-                        <a href=""><img class="w-100" height="100px" width="100px"src="img/gallery-2.jpg" alt=""></a>
-                    </div>
-                    <div class="col-4 px-1 mb-2">
-                        <a href=""><img class="w-100" height="100px" width="100px"src="img/gallery-3.jpg" alt=""></a>
-                    </div>
-                    <div class="col-4 px-1 mb-2">
-                        <a href=""><img class="w-100"height="100px" width="100px" src="img/gallery-4.jpg" alt=""></a>
-                    </div>
-                    <div class="col-4 px-1 mb-2">
-                        <a href=""><img class="w-100" height="100px" width="100px"src="img/gallery-5.jpg" alt=""></a>
-                    </div>
-                    <div class="col-4 px-1 mb-2">
-                        <a href=""><img class="w-100" height="100px" width="100px"src="img/gallery-6.jpg" alt=""></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 mb-5">
-                <h4 class="text-uppercase text-light mb-4">Haberdar Olayım</h4>
-                <p class="mb-4">Yeni gelen ilanları ve kampanyaları sizinle paylaşalım</p>
-                <div class="w-100 mb-3">
-                    <div class="input-group">
-                        <input type="text" class="form-control bg-dark border-dark" style="padding: 25px;" placeholder="Email Giriniz">
-                        <div class="input-group-append">
-                            <button class="btn btn-primary text-uppercase px-3">Kayıt Ol</button>
-                        </div>
-                    </div>
-                </div>
-                <i>Dilediğiniz zaman iptal edin</i>
+ <div class="container-fluid bg-secondary py-5 px-sm-3 px-md-5" style="margin-top: 90px;">
+    <div class="row pt-5">
+        <div class="col-lg-3 col-md-6 mb-5">
+            <h4 class="text-uppercase text-light mb-4">Contactez-nous</h4>
+            <p class="mb-2"><i class="fa fa-map-marker-alt text-white mr-3"></i>Campus unviersaitaire - Manar - Tunis<br> Tunisie</p>
+            <p class="mb-2"><i class="fa fa-phone-alt text-white mr-3"></i>+216 99 485 200</p>
+            <p><i class="fa fa-envelope text-white mr-3"></i>nosvoitures.com
+            </p>
+            <h6 class="text-uppercase text-white py-2">nos réseaux sociaux</h6>
+            <div class="d-flex justify-content-start">
+                <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="https://twitter.com/aitchkays"><i class="fab fa-twitter"></i></a>   
+                <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="https://www.facebook.com/profile.php?id=100018305796102&mibextid=LQQJ4d"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-lg btn-dark btn-lg-square mr-2" href="https://www.linkedin.com/in/halil-k%C3%B6se-7017bb1b4/"><i class="fab fa-linkedin-in"></i></a>
+                <a class="btn btn-lg btn-dark btn-lg-square" href="https://www.instagram.com/aitchkays/""><i class="fab fa-instagram"></i></a>
             </div>
         </div>
-    </div>
-    <div class="container-fluid bg-dark py-4 px-sm-3 px-md-5">
-        <p class="mb-2 text-center text-body">&copy; <a href="#">adamindan.com</a>. Tüm Hakları Saklıdır.</p>
-        
-        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->                 
-        <p class="m-0 text-center text-body">Designed by Halil Köse , Eyüp Terzi</a></p>
-    </div>
-    <!-- Footer End -->
 
+
+
+         <div class="col-lg-3 col-md-6 mb-5">
+            <h4 class="text-uppercase text-light mb-4"></h4>
+            <div class="d-flex flex-column justify-content-start">
+                <a class="text-body mb-2" href="http://localhost/demo/adamindan.com/contact.php"><i class="fa fa-angle-right text-white mr-2"></i></a>
+                <a class="text-body mb-2" href="http://localhost/demo/adamindan.com/contact.php"><i class="fa fa-angle-right text-white mr-2"></i></a>
+                <a class="text-body mb-2" href="http://localhost/demo/adamindan.com/contact.php"><i class="fa fa-angle-right text-white mr-2"></i></a>
+                <a class="text-body mb-2" href="http://localhost/demo/adamindan.com/contact.php"><i class="fa fa-angle-right text-white mr-2"></i></a>
+                <a class="text-body" href="http://localhost/demo/adamindan.com/contact.php"><i class="fa fa-angle-right text-white mr-2"></i></a>
+            </div>
+        </div>            <div class="col-lg-3 col-md-6 mb-5">
+            <h4 class="text-uppercase text-light mb-4">Galerie</h4>
+            <div class="row mx-n1">
+                <div class="col-4 px-1 mb-2">
+                    <a href=""><img class="w-100" height="100px" width="100px" src="img/gallery-1.jpg" alt=""></a>
+                </div>
+                <div class="col-4 px-1 mb-2">
+                    <a href=""><img class="w-100" height="100px" width="100px"src="img/gallery-2.jpg" alt=""></a>
+                </div>
+                <div class="col-4 px-1 mb-2">
+                    <a href=""><img class="w-100" height="100px" width="100px"src="img/gallery-3.jpg" alt=""></a>
+                </div>
+                <div class="col-4 px-1 mb-2">
+                    <a href=""><img class="w-100"height="100px" width="100px" src="img/gallery-4.jpg" alt=""></a>
+                </div>
+                <div class="col-4 px-1 mb-2">
+                    <a href=""><img class="w-100" height="100px" width="100px"src="img/gallery-5.jpg" alt=""></a>
+                </div>
+                <div class="col-4 px-1 mb-2">
+                    <a href=""><img class="w-100" height="100px" width="100px"src="img/gallery-6.jpg" alt=""></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5">
+            <h4 class="text-uppercase text-light mb-4">Tenez moi informé</h4>
+            <p class="mb-4">Partageons avec vous de nouvelles publications et campagnes</p>
+            <div class="w-100 mb-3">
+                <div class="input-group">
+                    <input type="text" class="form-control bg-dark border-dark" style="padding: 25px;" placeholder="Tapez votre mail">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary text-uppercase px-3">S'identifier</button>
+                    </div>
+                </div>
+            </div>
+            <i>Annulez quand vous voulez</i>
+        </div>
+    </div>
+</div>
+<div class="container-fluid bg-dark py-4 px-sm-3 px-md-5">
+    <p class="mb-2 text-center text-body">&copy; <a href="#">nosvoitures.com</a>.Tous droits résérvés.</p>
+    
+    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->                 
+    <p class="m-0 text-center text-body">Créé par GLSI2</a></p>
+</div>
+<!-- Footer End -->
 
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="fa fa-angle-double-up"></i></a>
